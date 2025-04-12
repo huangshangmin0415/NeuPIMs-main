d0b5528469d6652a9c9cb0d26c4352f8c103afac
hsm0415 <155465788@qq.com>
2025-04-12 19:47:01 +0800

    first stage sh is done, and 45 row second sh is here

--- brun.sh
+++ brun.sh
@@ -23,11 +23,11 @@ cd run
 # sh 203.sh > 203.txt 2>&1
 
 
-sh 210.sh > 210.txt 2>&1
-sh 211.sh > 211.txt 2>&1
+# sh 210.sh > 210.txt 2>&1
+# sh 211.sh > 211.txt 2>&1
 
-sh 212.sh > 212.txt 2>&1
-sh 213.sh > 213.txt 2>&1
+# sh 212.sh > 212.txt 2>&1
+# sh 213.sh > 213.txt 2>&1
 
 # sh 020.sh > 020.txt 2>&1
 # sh 021.sh > 021.txt 2>&1
@@ -70,3 +70,50 @@ sh 213.sh > 213.txt 2>&1
 # sh 241.sh > 241.txt 2>&1
 # sh 242.sh > 242.txt 2>&1
 # sh 243.sh > 243.txt 2>&1
+
+
+sh 004.sh > 004.txt 2>&1
+sh 005.sh > 005.txt 2>&1
+sh 006.sh > 006.txt 2>&1
+sh 014.sh > 014.txt 2>&1
+sh 015.sh > 015.txt 2>&1
+sh 016.sh > 016.txt 2>&1
+sh 024.sh > 024.txt 2>&1
+sh 025.sh > 025.txt 2>&1
+sh 026.sh > 026.txt 2>&1
+sh 034.sh > 034.txt 2>&1
+sh 035.sh > 035.txt 2>&1
+sh 036.sh > 036.txt 2>&1
+sh 044.sh > 044.txt 2>&1
+sh 045.sh > 045.txt 2>&1
+sh 046.sh > 046.txt 2>&1
+sh 104.sh > 104.txt 2>&1
+sh 105.sh > 105.txt 2>&1
+sh 106.sh > 106.txt 2>&1
+sh 114.sh > 114.txt 2>&1
+sh 115.sh > 115.txt 2>&1
+sh 116.sh > 116.txt 2>&1
+sh 124.sh > 124.txt 2>&1
+sh 125.sh > 125.txt 2>&1
+sh 126.sh > 126.txt 2>&1
+sh 134.sh > 134.txt 2>&1
+sh 135.sh > 135.txt 2>&1
+sh 136.sh > 136.txt 2>&1
+sh 144.sh > 144.txt 2>&1
+sh 145.sh > 145.txt 2>&1
+sh 146.sh > 146.txt 2>&1
+sh 204.sh > 204.txt 2>&1
+sh 205.sh > 205.txt 2>&1
+sh 206.sh > 206.txt 2>&1
+sh 214.sh > 214.txt 2>&1
+sh 215.sh > 215.txt 2>&1
+sh 216.sh > 216.txt 2>&1
+sh 224.sh > 224.txt 2>&1
+sh 225.sh > 225.txt 2>&1
+sh 226.sh > 226.txt 2>&1
+sh 234.sh > 234.txt 2>&1
+sh 235.sh > 235.txt 2>&1
+sh 236.sh > 236.txt 2>&1
+sh 244.sh > 244.txt 2>&1
+sh 245.sh > 245.txt 2>&1
+sh 246.sh > 246.txt 2>&1
