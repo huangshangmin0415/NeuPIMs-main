a920ae4185d6765f562f6c850b6745c69b78110f
hsm0415 <155465788@qq.com>
2025-04-12 19:27:28 +0800

    first add a brun.sh

---
+++ brun.sh
@@ -0,0 +1,72 @@
+cd run
+
+# sh 000.sh > 000.txt 2>&1
+# sh 001.sh > 001.txt 2>&1
+# sh 010.sh > 010.txt 2>&1
+# sh 011.sh > 011.txt 2>&1
+# sh 100.sh > 100.txt 2>&1
+# sh 101.sh > 101.txt 2>&1
+# sh 110.sh > 110.txt 2>&1
+# sh 111.sh > 111.txt 2>&1
+# sh 200.sh > 200.txt 2>&1
+# sh 201.sh > 201.txt 2>&1
+
+# sh 002.sh > 002.txt 2>&1
+# sh 003.sh > 003.txt 2>&1
+# sh 012.sh > 012.txt 2>&1
+# sh 013.sh > 013.txt 2>&1
+# sh 102.sh > 102.txt 2>&1
+# sh 103.sh > 103.txt 2>&1
+# sh 112.sh > 112.txt 2>&1
+# sh 113.sh > 113.txt 2>&1
+# sh 202.sh > 202.txt 2>&1
+# sh 203.sh > 203.txt 2>&1
+
+
+sh 210.sh > 210.txt 2>&1
+sh 211.sh > 211.txt 2>&1
+
+sh 212.sh > 212.txt 2>&1
+sh 213.sh > 213.txt 2>&1
+
+# sh 020.sh > 020.txt 2>&1
+# sh 021.sh > 021.txt 2>&1
+# sh 030.sh > 030.txt 2>&1
+# sh 031.sh > 031.txt 2>&1
+# sh 120.sh > 120.txt 2>&1
+# sh 121.sh > 121.txt 2>&1
+# sh 130.sh > 130.txt 2>&1
+# sh 131.sh > 131.txt 2>&1
+# sh 220.sh > 220.txt 2>&1
+# sh 221.sh > 221.txt 2>&1
+
+# sh 022.sh > 022.txt 2>&1
+# sh 023.sh > 023.txt 2>&1
+# sh 032.sh > 032.txt 2>&1
+# sh 033.sh > 033.txt 2>&1
+# sh 122.sh > 122.txt 2>&1
+# sh 123.sh > 123.txt 2>&1
+# sh 132.sh > 132.txt 2>&1
+# sh 133.sh > 133.txt 2>&1
+# sh 222.sh > 222.txt 2>&1
+# sh 223.sh > 223.txt 2>&1
+
+
+# sh 230.sh > 230.txt 2>&1
+# sh 231.sh > 231.txt 2>&1
+
+# sh 232.sh > 232.txt 2>&1
+# sh 233.sh > 233.txt 2>&1
+
+# sh 040.sh > 040.txt 2>&1
+# sh 041.sh > 041.txt 2>&1
+# sh 042.sh > 042.txt 2>&1
+# sh 043.sh > 043.txt 2>&1
+# sh 140.sh > 140.txt 2>&1
+# sh 141.sh > 141.txt 2>&1
+# sh 142.sh > 142.txt 2>&1
+# sh 143.sh > 143.txt 2>&1
+# sh 240.sh > 240.txt 2>&1
+# sh 241.sh > 241.txt 2>&1
+# sh 242.sh > 242.txt 2>&1
+# sh 243.sh > 243.txt 2>&1
