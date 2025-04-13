ef02623866e311e655dcf27ddc9176a78a9a70af
hsm0415 <155465788@qq.com>
2025-04-13 12:36:02 +0800

    before 45 llama; data, sh and model_config

---
+++ run/216.sh
@@ -0,0 +1,41 @@
+cd ../build; make; cd ..;
+
+# config file
+config=./configs/systolic_ws_128x128_dev.json
+mem_config=./configs/memory_configs/neupims.json
+model_config=./configs/model_configs/llama2-70B.json
+sys_config=./configs/system_configs/sub-batch-on.json
+cli_config=./data/share-gpt-prev-lmbs128-ms70B-tp16-pp4-clb-0.csv
+
+# log file
+LOG_LEVEL=info
+DATE=$(date "+%F_%H:%M:%S")
+
+# get the standard log dir
+name="$0"
+base_name=$(basename "$name")
+folder_name="${base_name%.*}"
+LOG_DIR=experiment_logs/${folder_name}
+
+mkdir -p $LOG_DIR;
+LOG_NAME=simulator.log
+CONFIG_FILE=${LOG_DIR}/config.log
+
+echo "log directory: $LOG_DIR"
+
+
+
+./build/bin/Simulator \
+    --config $config \
+    --mem_config $mem_config \
+    --cli_config $cli_config \
+    --model_config $model_config \
+    --sys_config $sys_config \
+    --log_dir $LOG_DIR
+
+
+echo "memory config: $mem_config" > ${CONFIG_FILE}
+echo "client config: $cli_config" >> ${CONFIG_FILE}
+echo "model config: $model_config" >> ${CONFIG_FILE}
+echo "system config: $sys_config" >> ${CONFIG_FILE}
+cat ${CONFIG_FILE}
